5a49be0dd1609c8dff6aadaa158ad7950d5497c9
Feather <liangzhi21@qq.com>
2020-05-26 16:34:54 +0800

    change

--- vantrules/src/App.vue
+++ vantrules/src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-
-
       <van-form @submit="onSubmit">
         <van-field
           v-model="form.quantity"
@@ -18,7 +15,7 @@
           input-align='right'
           @change="changeQuantity"
         />
-        <div v-for="(item) in packingList" :key="item.id">
+        <div v-for="(item, index) in packingList" :key="item.id">
           <van-field
             readonly
             v-model="item.stock"
@@ -38,7 +35,7 @@
             label="领取数量"
             placeholder="请输入（个）"
             input-align='right'
-            :rule="getRule(1)"
+            :rules="getRule(index)"
           />
         </div>
         <div style="margin: 16px 16px 0;">
@@ -65,15 +62,7 @@ export default {
         quantity: '',
         sampleQuantity: ''
       },
-      packingList:[
-      ],
-      rules: {
-        packQuantity: [
-          { required: true, message: '请填写领取数量', trigger: 'onChange' },
-          { validator: this.validatePositiveInteger, message: '只能输入整数', trigger: 'onChange' },
-          { validator: this.validatorGreenBeanGoodQuantity, message: '包材领取数量不能大于余量', trigger: 'onChange' }
-        ]
-      },
+      packingList:[],
       arr: [
         {
           stock: 60,
@@ -99,25 +88,18 @@ export default {
       return /^\+?[0-9][0-9]*$/.test(value)
     },
     getRule(index) {
-      console.log('getRule');
-      return {
-        index,
-        validator: this.validator
-      };
+      return [
+        {index, validator: this.validator, message: '包材领取数量不能大于余量', trigger: 'onChange'},
+        { required: true, message: '请填写领取数量', trigger: 'onChange' },
+        { validator: this.validatePositiveInteger, message: '只能输入整数', trigger: 'onChange' }
+      ];
     },
     validator(value, rule) {
-      console.log('validator')
-      console.log(rule.index);
+      console.log(value, rule)
+      return this.packingList[rule.index].countQuantity <= this.packingList[rule.index].stock;
     },
     validatorGreenBeanGoodQuantity(value) {
       console.log(value)
-      /** 
-      * 当我想找出现在修改的然后进行匹配，会发生以下情况
-      *  操作实例：  生产总数输入22  留样数量 输入22   然后两个领取数量都报错，然后修改不满足条件的那个领取数量， 另一个一直显示错误，无法消除， 只能再修改一次另一个才可以消除错误
-      *  因为是由另外两个不相关的值计算得到的，两个值的领取数量都一致，由于没有返回索引，没办法拿到具体的某一项
-      *  没办法拿到具体某一项就会出现一个不匹配，几项全报错的情况。
-      *  这里需要一个 index  值
-      *  */ 
       return this.packingList.every(item=>{
         return item.countQuantity <= item.stock;
       })
@@ -130,14 +112,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
